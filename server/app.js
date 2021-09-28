88af529700fdb333576ff408780648e31c30fa77
ZNBrown <ZachNatBrown@gmail.com>
2021-09-28 14:49:35 +0100

    stashing changes

--- server/app.js
+++ server/app.js
@@ -5,3 +5,14 @@ app.use(cors());
 
 app.get('/', (req, res) => res.json(message = "Not google"))
 app.listen(3000, () => console.log(`\nServer launched port 3000!\n`))
+
+
+app.get('/search', (req, res) => {
+
+})
+
+app.get('/search/:term', (req, res) => {
+    let searchTerm = req.params.term;
+
+    
+})
